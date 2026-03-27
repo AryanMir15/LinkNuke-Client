9d1c29a2f536baff8f3f14e7d61842cc16a7ddd1
AryanMir15 <aryanmir115@gmail.com>
2026-03-27 11:58:47 +0500

    Went back to the /dashboard routing

--- astro/src/react-app/ReactApp.jsx
+++ astro/src/react-app/ReactApp.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 export default function ReactApp() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/dashboard">
       <AppWrapper />
     </BrowserRouter>
   );
